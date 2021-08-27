3129f00bf14ba22c8843fb866efd29dbe8d78fce
EvgeniyKravchuk <gekahhh1996@gmail.com>
2021-08-27 13:47:09 +0300

    finally

--- src/API/axios-default-options.js
+++ src/API/axios-default-options.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3004";
+axios.defaults.baseURL = "http://localhost:3001";
 axios.defaults.headers.post["Content-Type"] = "application/json";
